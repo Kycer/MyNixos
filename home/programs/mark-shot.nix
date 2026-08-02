a1c9f3777d490216cf003cdc43cd776d453bebb0
Kycer <978262587@qq.com>
2026-08-02 12:42:42 +0800

    change

--- home/programs/mark-shot.nix
+++ home/programs/mark-shot.nix
@@ -1,9 +1,15 @@
 { inputs, lib, osConfig, pkgs, ... }:
 
+let
+  markShot = inputs.mark-shot.packages.${pkgs.stdenv.hostPlatform.system}.default;
+in
 {
   config = lib.mkIf (osConfig.my.desktop.wm == "sway") {
     home.packages = [
-      inputs.mark-shot.packages.${pkgs.stdenv.hostPlatform.system}.default
+      (pkgs.writeShellScriptBin "mark-shot" ''
+        export XDG_SESSION_TYPE=wayland
+        exec ${lib.getExe markShot} "$@"
+      '')
     ];
   };
 }
